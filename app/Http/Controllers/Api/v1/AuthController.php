417f52c000f8e2187f01558dbee6cbf5652af08b
Jaydeep Dhrangiya <recentpagetoday@gmail.com>
2025-02-02 11:34:48 +0530

    sa

--- app/Http/Controllers/Api/v1/AuthController.php
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
@@ -139,9 +140,11 @@ class AuthController extends BaseController
                         ]
                     ]);
 
+            Log::info($response->json());
+
             return $this->sendResponse([
                 'message' => 'OTP sent successfully',
-                'expires_in' => 5 // minutes
+                'expires_in' => 10 // minutes
             ], 'OTP sent successfully');
         } catch (\Exception $e) {
             logError('AuthController', 'phoneLogin', $e->getMessage());
